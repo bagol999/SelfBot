d4e5c528c90f094f6ce8cb2f75568e0844543844
bagil999 <andreaseka145@gmail.com>
2021-11-13 10:39:31 +0700

    SelfBot

---
+++ cara install.js
@@ -0,0 +1,50 @@
+/*
+
+
+
+> termux-setup-storage
+> apt update && apt upgrade
+> pkg install nodejs
+> pkg install git 
+> pkg install imagemagick
+> pkg install libwebp 
+> pkg install bash
+> pkg install ffmpeg
+> cd /sdcard && cd SelfBot && npm start
+
+
+extrack file botnya kemudian taroh folder selfbot
+di internal memori
+
+kemudian buka termuxnya
+
+copy satu satu kode di atas bagi yg pertama kali
+pake termux
+
+bagi yg udah pernah install bot sebelumnya tinggal
+paste kode di bawah ini :
+
+cd /sdcard && cd SelfBot && npm start
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
